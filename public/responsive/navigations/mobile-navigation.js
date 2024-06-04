d2667eb81fa2709e96a8f918e46935fc8a78b6a5
priyanshu Chandra <chandrapriyanshu10@gmail.com>
2024-06-05 04:41:47 +0530

    7th commit

---
+++ public/responsive/navigations/mobile-navigation.js
@@ -0,0 +1,10 @@
+on
+function mobile_navigations_toggleopen(){
+   var toggle_button =  document.getElementById('mobile-navigation-sidebar');
+   toggle_button.style.display = 'block';
+    
+}
+function mobile_navigations_toggleclose(){
+    var toggle_button =  document.getElementById('mobile-navigation-sidebar');
+    toggle_button.style.display = 'none';
+ }
